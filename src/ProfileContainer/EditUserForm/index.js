2f18e684cddce3af8a4944cd348688fae11d076f
Nehemias Alcantara <nemiasalc56@gmail.com>
2020-02-29 19:31:04 -0600

    edit form user account has a form

--- src/ProfileContainer/EditUserForm/index.js
+++ src/ProfileContainer/EditUserForm/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Button, Form, Grid, Segment } from 'semantic-ui-react'
 
 
 class EditUserForm extends Component {
@@ -6,9 +7,94 @@ class EditUserForm extends Component {
 
 	render() {
 		return(
-			<div>
-				<h3>EditUserForm</h3>
-			</div>
+			<Grid className="center aligned">
+					<Segment 
+						style={{
+							marginTop: '200px'
+						}}
+					>
+						<Grid.Row>
+							<h2>Update your account information</h2>
+						</Grid.Row>
+						<Form >
+							
+								<Form.Group>
+								<Form.Input 
+									label="First name"
+									type="text" 
+									name="first_name"
+									placeholder="First name" />
+								<Form.Input 
+									label="Last name"
+									type="text"
+									name="last_name"
+									placeholder="Last name" />
+							</Form.Group>
+							<Form.Group>
+								
+								<Form.Input 
+									label="Address 1"
+									type="text" 
+									name="address_1"
+									placeholder="Address 1" />
+							
+								<Form.Input 
+									label="Address 2" 
+									type="text" 
+									name="address_2"
+									placeholder="Address 2" />
+							
+								<Form.Input 
+									label="City"
+									type="text" 
+									name="city"
+									placeholder="City" />
+							
+								<Form.Input 
+									label="State" 
+									type="text" 
+									name="state"
+									placeholder="state" />
+							
+								<Form.Input 
+									label="Zipcode"
+									type="text" 
+									name="zip_code"
+									placeholder="Zipcode" />
+							</Form.Group>
+
+							<Form.Field>
+								<Form.Input 
+									label="Image"
+									type="text" 
+									name="picture"
+									placeholder='Image' />
+							</Form.Field>
+
+							<Form.Field>
+								<label>Email</label>
+								<input 
+									type="text" 
+									name="email"
+									placeholder="Enter email" />
+							</Form.Field>
+
+							<Form.Field>
+								<label>Password</label>
+								<input 
+									type="password" 
+									name="password"
+									placeholder="Enter password" />
+							</Form.Field>
+
+							<Button color="green" className="big login-button" type='submit'>
+								Update Account
+							</Button>
+							
+						</Form>
+					
+					</Segment>
+				</Grid>
 			)
 	}
 }
