7433e573b93bf552f9a1065776f122848aeb6718
Nehemias Alcantara <nemiasalc56@gmail.com>
2020-03-04 12:43:23 -0600

    user is able to delete a comment

--- src/ItemsContainer/CommentsContainer/index.js
+++ src/ItemsContainer/CommentsContainer/index.js
@@ -55,40 +55,66 @@ class CommentContainer extends Component {
 		e.preventDefault()
 		// set up url with the item id
 		const url = process.env.REACT_APP_API_URL + '/api/v1/comments/' + this.props.item.id
-
-		try {
-			const commentResponse = await fetch(url, {
-				credentials: 'include',
-				method: 'POST',
-				body: JSON.stringify(this.state),
-				headers: {
-					'Content-Type': 'application/json'
-				}
-			})
-
-			const commentJson = await commentResponse.json()
-
-			if(commentJson.status === 200) {
-				// this is so that we can see the comment that was added showing on the page
-				const newCommentArray = this.state.commentList
-				newCommentArray.push(commentJson.data)
-
-				this.setState({
-					commentList: newCommentArray,
-					comment: ''
+		if(this.state.comment !== '') {
+			try {
+				const commentResponse = await fetch(url, {
+					credentials: 'include',
+					method: 'POST',
+					body: JSON.stringify(this.state),
+					headers: {
+						'Content-Type': 'application/json'
+					}
 				})
-			}
 
-		} catch(err) {
-			console.error(err);
+				const commentJson = await commentResponse.json()
+
+				if(commentJson.status === 200) {
+					// this is so that we can see the comment that was added showing on the page
+					const newCommentArray = this.state.commentList
+					newCommentArray.push(commentJson.data)
+
+					this.setState({
+						commentList: newCommentArray,
+						comment: ''
+					})
+				}
+
+			} catch(err) {
+				console.error(err);
+			}
+			
 		}
 	}
 
 	// delete a comment
-	deleteComment = (commentToDeleteId) => {
+	deleteComment = async (commentToDeleteId) => {
 		// set up our url
 		const url = process.env.REACT_APP_API_URL + '/api/v1/comments/' + commentToDeleteId
-		console.log(commentToDeleteId);
+		
+		const deleteCommentResponse = await fetch(url, {
+			credentials: 'include',
+			method: 'DELETE',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+		const deleteCommentJson = await deleteCommentResponse.json()
+		
+		// remove the item from my array of comments in state
+		if(deleteCommentJson.status === 200) {
+			const commentList = this.state.commentList
+			let index = 0
+			// find the comment that was delete in state
+			for(let i = 0; i < commentList.length; i++) {
+				if(commentList[i].id === commentToDeleteId){
+					index = i
+				}
+			}
+			// remove the comment with that index from state
+			commentList.splice(index, 1)
+			this.setState({commentList: commentList})
+			this.getComments()
+		}
 	}
 
 
